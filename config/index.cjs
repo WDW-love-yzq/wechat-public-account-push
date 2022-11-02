ace126967d5ac8eb2d8b8499f79f9850bcfeeacd
WDW-love-yzq <97928402+WDW-love-yzq@users.noreply.github.com>
2022-11-02 13:21:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -10,17 +10,17 @@ const USER_CONFIG = {
   // 使用【微信测试号】请填写 wechat-test
   // 使用【方糖服务号】请填写 server-chan
   // 使用【pushplus推送加服务号】请填写 push-plus
-  //USE_PASSAGE: 'push-plus',
-  USE_PASSAGE: 'wechat-test',
+  USE_PASSAGE: 'push-plus',
+  //USE_PASSAGE: 'wechat-test',
   // 使用微信测试号时才需要填写：公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: 'wx1ed465f898695b9d',
+  APP_ID: '',
 
   // 使用微信测试号时才需要填写：公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '64fb1b9d1046fca6fbea96a604372554',
+  APP_SECRET: '',
   
   // 为了避免推送服务器误将脚本列为恶意推送脚本，可设置每分钟脚本最大推送数
   // 每分钟脚本最大推送数，超过此数将会休眠1分钟后再发送剩余消息，不填则默认为5
@@ -111,8 +111,7 @@ const USER_CONFIG = {
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       // 使用其他通道时，请严格按照各个通道的教程进行填写
-      //id: '29a331f10b40410eaf836ed128934c04',
-      id:'oHnqO56dNYxkuvogr0mga2cNJKwE',
+      id: '29a331f10b40410eaf836ed128934c04',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
       useTemplateId: '0001',
